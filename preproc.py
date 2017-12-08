5cd13346e55a59285e0a96a0df7cb2aef49f34ef
蔡丰宇 <caifengyu@caifengyudeMacBook-Pro.local>
2017-12-08 12:31:00 +0800

    finish list

--- preproc.py
+++ preproc.py
@@ -21,12 +21,14 @@ def _wn_tag(tag):
 def get_tag(word):
 	word_tok = word_tokenize(word)
 	tag = pos_tag(word_tok)
-
 	tag_list = [_wn_tag(i[1]) for i in tag]
-	
+
+	return tag_list
+
 def preprocess(word):
-	
-	
+	# get the tag
+	tag_list = get_tag(word)
+
 	word_ = word.replace(' ', '_')
 	file =word_ + '.txt'
 	filename = os.path.join(dir_path, 'data', file)
@@ -35,11 +37,29 @@ def preprocess(word):
 		print('file not found:', file)
 
 	with io.open(filename, 'r') as file:
-		file = file.readlines()
+		text = file.readlines()
 
-	file
-	return file
+	word_list = []
+	for i in text:
+		new_line = i.replace('\n', '').split()
+		word_list.append(new_line)
+	
+	word_list_new = [[word, tag_list, i[-3], i[-2], i[-1]] for i in word_list]
+	return word_list_new
 
 if __name__ == "__main__":
-	a = preprocess("artificial intelligence")
-	print(a)
+	# get the word list
+	with io.open('wordlist.txt', 'r') as file:
+		wordlist = file.readlines()
+
+	wordlist = [i.replace('\n', '') for i in wordlist]
+
+	final_list = []
+	for name in wordlist:
+		final_list += preprocess(name)
+	
+	output = open('output.txt', 'w+')
+	for i in final_list:
+		output.write(str(i))
+
+	output.close()
